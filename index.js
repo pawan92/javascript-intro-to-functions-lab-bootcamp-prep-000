c5c3c5ed2ae400e7269712e081f7264456b2e8b5
Pawan M <pawanmurthy92@gmail.com>
2018-02-03 17:01:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-function shout(string) {
-return string.toUpperCase();
-  
-}
 
-function whisper(string) {
-return string.toLowerCase();
-  
-}
-
-function logShout(string) {
-console.log (string.toUpperCase());
-  
-}
-
-function logWhisper(string) {
-console.log (string.toLowerCase());
-  
-}
 
 function sayHiToGrandma(string) {
   if(string.toLowerCase() === string)
@@ -30,9 +12,4 @@ function sayHiToGrandma(string) {
   
 }
 
-function sayHiToGrandma(string) {
-  if(string === "I love you, Grandma.")
-    return "I love you, too."
-  
-}
 
