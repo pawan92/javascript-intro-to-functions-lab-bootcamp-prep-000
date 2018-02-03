b5ceeea1a02b54a772145b0de8b1277af0b43dd4
Pawan M <pawanmurthy92@gmail.com>
2018-02-03 16:51:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,5 +24,15 @@ function sayHiToGrandma(string) {
   
 }
 
+function sayHiToGrandma(string) {
+  if(string.toUpperCase() === string)
+    return "YES INDEED!"
+  
+}
 
+function sayHiToGrandma(string) {
+  if(string === "I love you, Grandma.")
+    return "I love you, too.!"
+  
+}
 
