44d6690d5a6b4b773d85aa18791ff3b87ef6f703
Pawan M <pawanmurthy92@gmail.com>
2018-02-03 17:08:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function sayHiToGrandma(string) {
   if(string.toLowerCase() === string)
-    return "I can't hear you!"
+    return "I can't hear you!'"
   
 }
 
@@ -28,7 +28,7 @@ console.log (string.toLowerCase());
 
 function sayHiToGrandma(string) {
   if(string.toUpperCase() === string)
-    return "YES INDEED!"
+    return 'YES INDEED!'
   
 }
 
